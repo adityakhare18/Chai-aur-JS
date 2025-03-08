8e9783ec27acc66c6abb6f618c972d2c1ae168c0
adityakhare18 <khareaditya0105@gmail.com>
2025-03-09 00:25:07 +0530

    Add explanation of stack and heap memory in JavaScript

---
+++ 10_Stack-Heap-Memory.js
@@ -0,0 +1,29 @@
+/*
+The Stack is a small, fast, and automatically managed memory area that stores primitive values and function execution context. It follows the LIFO (Last In, First Out) principle.
+
+Stack in JavaScript
+Stores:
+
+Primitive data types (e.g., number, string, boolean, null, undefined, symbol, bigint).
+Function calls and local variables.
+Execution contexts for function calls.
+*/
+
+/*
+The Heap is a large, dynamically allocated memory area that stores objects and reference types.
+
+Heap in JavaScript
+Stores:
+
+Objects ({}), Arrays ([]), Functions.
+Memory is allocated dynamically and managed by Garbage Collection (GC).
+How it works:
+
+When an object or array is created, it is stored in heap memory.
+Variables in the stack store references (pointers) to these objects in the heap.
+*/
+
+
+//stack => primitive dt, hamehsa copy bnti h
+
+//heap => reference dt, copy nhi bnti h, reference bnti h

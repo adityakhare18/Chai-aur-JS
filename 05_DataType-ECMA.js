68fe8d83780b26a05cebc6a57543ae5c3bb41857
adityakhare18 <khareaditya0105@gmail.com>
2025-03-08 23:34:05 +0530

    DataType in JS and its type

---
+++ 05_DataType-ECMA.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version // not necessary to write becouse inbuilt aa jata h
+
+// 1. Data Types in JS 
+// primitive data types
+// number => 2 to the powwer of 53
+// bigint
+// string => " " or ' ' or ` `
+// boolean => true or false
+// null => empty/standalone value
+// undefined => variable declared but not assigned
+// symbol => unique value
+
+//console.log(typeof null); //object
+//console.log(typeof undefined); //undefined
+
+// object data types

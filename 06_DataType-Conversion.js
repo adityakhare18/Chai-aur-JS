11fcf1932cff9ad4bc9b2955b9cd1672a7c8226e
adityakhare18 <khareaditya0105@gmail.com>
2025-03-08 23:44:17 +0530

    Add examples for data type conversion in JavaScript

---
+++ 06_DataType-Conversion.js
@@ -0,0 +1,25 @@
+// let score = "33";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = "adi"
+
+//console.log(typeof score); 
+//          OR
+//console.log(typeof (score)); 
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber); //but variable me value jayegi
+console.log(valueInNumber); 
+
+
+// "33" => 33
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+// "adi" => NaN
+// "33adi" => NaN
+// 1 => true if convert to Boolean
+// 0 => false if convert to Boolean
+// adi => true if convert to Boolean

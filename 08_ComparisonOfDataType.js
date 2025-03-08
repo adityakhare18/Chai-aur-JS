0bdce97292df725243be775d92bf5a020c6b4487
adityakhare18 <khareaditya0105@gmail.com>
2025-03-09 00:02:14 +0530

    Add examples demonstrating comparison behavior of null and undefined in JavaScript

---
+++ 08_ComparisonOfDataType.js
@@ -0,0 +1,22 @@
+//console.log(2 > "1"); //true
+//console.log(2 < "1"); //false
+//console.log(2 == "2"); //true
+//console.log(2 === "2"); //false
+
+console.log(null > 0); //false
+console.log(null == 0);// false
+console.log(null >= 0); //true
+
+// == and >,<,>=,<= works differencely with null,
+// comparison with null convert to number treateing null as 0
+// null > -1 => 0 > -1 => true
+
+// comparison operator number me convert kr deta h
+
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+
+// undefined hamesa false dega

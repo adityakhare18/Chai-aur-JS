32db8e42e3af8f17122d509db0f1551db9e61a6c
adityakhare18 <khareaditya0105@gmail.com>
2025-03-08 23:54:40 +0530

    Add examples for string to number conversion in JavaScript

---
+++ 07_String2Number-Conversion.js
@@ -0,0 +1,24 @@
+let value = 3;
+let negValue = -value
+//console.log(negValue,value); -3 , 3
+//console.log(typeof negValue,typeof value); //number number
+
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+//console.log(str3); //Hell oWorld
+
+//console.log("1" + 2); //12 string
+//console.log(1 + "2"); // 12 string type
+
+//console.log("1" + 2 + 3); //123 string
+//console.log(1 + 2 + "3"); //33 string
+
+//left to right chl rha tha upar
+
+//console.log(true); //true
+//console.log(+true); //1
+//console.log(+false); //0
+//console.log(true+); //syntax error
+
